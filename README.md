f4847086719a6ddd788b3bdf00280e738a2dba40
Lea Droguet <ljdroguet@gmail.com>
2021-09-28 15:24:59 +0000

    use IAM roles instead of user

--- README.md
+++ README.md
@@ -15,50 +15,26 @@
 
 ## Description
 
-This plugin will retrieve Amazon AWS EC2 instances details using the describe-instances command of the AWS CLI.
+This plugin will retrieve Amazon AWS EC2 instances details using the describe-instances and STS AssumeRole commands of the AWS CLI.
 
 ## Prerequisites
 
 *The following configuration has to be available :*
-1. An AWS IAM user with AmazonEC2ReadOnlyAccess on the AWS infrastructure
-2. AWS CLI installed, see : https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html
-
-
-*AWS CLI Configuration :*
-
-Configure an AWS CLI profile named 'ocs'.
-```
->> aws configure --profile ocs
-AWS Access Key ID [None]: ********************
-AWS Secret Access Key [None]: *******************************
-Default region name [None]: None
-Default output format [None]: json
-```
-
-Enter your user's **access key ID** and **secret access key** provided at the user creation on the AWS management console.
-
-Enter 'None' for the default region name. You will be able to add one or multiple regions inside **agent/Awsressources.pm**, see below for more details.
-
-**JSON output is needed** for the plugin to work.
+1. An IAM user configured with AmazonEC2ReadOnlyAccess on the AWS infrastructure and STS AssumeRole authorizations 
+2. IAM roles with trust policies for the above user on the accounts you wish to scan for AWS resources
+3. AWS CLI installed, see : https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html
 
 
 ## Configuration
 
-If you chose a different profile name than 'ocs' during the previous step, please edit the file **agent/Awsressources.pm** with your profile name : 
+Please edit the file **agent/Awsressources.pm** with your target account id(s) and target regions: 
 
-Line 46 :  
+Line 47 to 49 :  
 ```
-my $profile = 'ocs';
+    # add there 1. targeted account and 2. arrays of targeted regions for this account
+    my %targets = (
+            '621784199302' =>    ['eu-central-1', 'us-east-2'],
+            '000000000001' =>    ['eu-central-2', 'us-east-1'],
+            ); 
 ```
-To choose which regions you want to search for instances, edit these lines : 
-
-Line 49 to 52 :  
-```
-my @regions = (
-            'us-east-2',
-            'us-east-1',
-            );
-```
-
-
 
--- agent/Unix/Awsressources.pm
+++ agent/Unix/Awsressources.pm
@@ -1,4 +1,4 @@
-# Plugin "Aws Ressources" OCSInventory
+ # Plugin "Aws Ressources" OCSInventory
 # Author: Léa DROGUET
 
 package Ocsinventory::Agent::Modules::Awsressources;
@@ -6,6 +6,7 @@ package Ocsinventory::Agent::Modules::Awsressources;
 use Encode qw(decode);
 use POSIX qw(strftime);
 use JSON::PP;
+use Data::Dumper;
 
 
 sub new {
@@ -42,101 +43,123 @@ sub awsressources_inventory_handler {
 
     $logger->debug("Yeah you are in awsressources_inventory_handler :)");
 
-    # Please modify below variable if you chose any other profile name during aws cli configuration
-    my $profileName = 'ocs';
-
-    # Define here which regions you wish to scan for instances (the profile above must have access to these)
-    my @regions = (
-                'us-east-2',
-                'us-east-1',
-                );
+    # add there 1. targeted account and 2. arrays of targeted regions for this account
+    my %targets = (
+            '000000000002' =>    ['eu-central-1', 'us-east-2'],
+            '000000000001' =>    ['eu-central-2', 'us-east-1'],
+            ); 
     
-    foreach my $region (@regions) {
-        # Instances will be retrieved for every region specified above
-        my $result = `aws ec2 --profile $profileName describe-instances --region $region`;
-        $result = decode_json $result;
+    # modify this to match your IAM role name
+    my $IAMrole = 'InventorizationRole';
+    
 
-        if (@{$result->{Reservations}}) {
-            $logger->debug("Generating xml data for region : $region");        
-            # reservations level
-            foreach my $reservation (@{$result->{Reservations}}) {
-                my $reservationId = $reservation->{ReservationId};
-                my $ownerId = $reservation->{OwnerId};
-                
-                # instance level
-                foreach my $instance (@{$reservation->{Instances}}) {
-                    # Add XML
-                    push @{$common->{xmltags}->{AWS_INSTANCES}},
-                    {
-                        RESERVATION_ID => [$reservationId],
-                        OWNER_ID => [$ownerId],
-                        INSTANCE_ID => [$instance->{InstanceId}],
-                        INSTANCE_TYPE => [$instance->{InstanceType}],
-                        LAUNCH_TIME => [$instance->{LaunchTime}],
-                        AVAILABILTY_ZONE => [$instance->{Placement}{AvailabilityZone}],
-                        ARCHITECTURE => [$instance->{Architecture}],
-                        KEY_NAME => [$instance->{KeyName}],
-                        IMAGE_ID => [$instance->{ImageId}],
-                        AMI_LAUNCH_INDEX => [$instance->{AmiLaunchIndex}],
-                        MONITORING => [$instance->{Monitoring}{State}],
-                        STATE_CODE => [$instance->{State}{Code}],
-                        STATE_NAME => [$instance->{State}{Name}],
-                        STATE_REASON => [$instance->{StateTransitionReason}],
-                        VIRTUALIZATION_TYPE => [$instance->{VirtualizationType}],
-                        HIBERNATION_OPT_CONFIGURED => [JSON::PP::is_bool($instance->{HibernationOptions}->{Configured})],
-                        ENCLAVED_OPT_ENABLED => [JSON::PP::is_bool($instance->{EnclaveOptions}->{Enabled})],
+    foreach my $account (keys %targets) {
+        $arn = "arn:aws:iam::$account:role/$IAMrole";
 
-                    };
+        if (($assumeRole = `aws sts assume-role --role-arn "$arn" --role-session-name "OCS-Inventorization" 2>/dev/null`) ne '') {
+            $res = decode_json $assumeRole;
+            $logger->debug("AssumeRole successful for ARN: $arn");
+            # set env var for this AWS role
+            $ENV{AWS_ACCESS_KEY_ID} = $res->{Credentials}{AccessKeyId};
+            $ENV{AWS_SECRET_ACCESS_KEY} = $res->{Credentials}{SecretAccessKey};
+            $ENV{AWS_SESSION_TOKEN} = $res->{Credentials}{SessionToken};
 
-                    foreach my $networkscat (@{$instance->{NetworkInterfaces}}) {
-                        push @{$common->{xmltags}->{AWS_INSTANCES_NETWORKS}},
-                        {
-                            RESERVATION_ID => [$reservationId],
-                            OWNER_ID => [$ownerId],
-                            INSTANCE_ID => [$instance->{InstanceId}],
-                            MAC_ADDR => [$networkscat->{MacAddress}],
-                            PRIVATE_DNS_NAME => [$networkscat->{PrivateDnsName}],
-                            PRIVATE_IP_ADDR => [$networkscat->{PrivateIpAddress}],
-                            PUBLIC_DNS_NAME => [$networkscat->{PublicDnsName}],
-                            VPC_ID => [$networkscat->{VpcId}],
-                            NETWORK_INTERFACE_ID => [$networkscat->{NetworkInterfaceId}],
-                            SUBNET_ID => [$networkscat->{SubnetId}],
-
-                        };
-
-                    }
-
-
-                    foreach my $blockcat (@{$instance->{BlockDeviceMappings}}) {
-                        push @{$common->{xmltags}->{AWS_INSTANCES_HARDWARE}},
-                        {
-                            RESERVATION_ID => [$reservationId],
-                            OWNER_ID => [$ownerId],
-                            INSTANCE_ID => [$instance->{InstanceId}],
-                            ROOT_DEVICE_NAME => [$instance->{RootDeviceName}],
-                            ROOT_DEVICE_TYPE => [$instance->{RootDeviceType}],
-                            CPU_CORE_COUNT => [$instance->{CpuOptions}{CoreCount}],
-                            CPU_THREADS_PER_CORE => [$instance->{CpuOptions}{ThreadsPerCore}],
-                            HYPERVISOR => [$instance->{Hypervisor}],
-                            BLOCK_DEVICE_NAME => [$blockcat->{DeviceName}],
-                            # BLOCK_DEVICE_TYPE => $blockcat->{Ebs},
-                            BLOCK_DEVICE_ATTACH_TIME => [$blockcat->{Ebs}{AttachTime}],
-                            BLOCK_DEVICE_DELETE_ON_TERMINATION => [JSON::PP::is_bool($blockcat->{Ebs}{DeleteOnTermination})],
-                            BLOCK_DEVICE_STATUS => [$blockcat->{Ebs}{Status}],
-                            BLOCK_DEVICE_VOLUME_ID => [$blockcat->{Ebs}{VolumeId}],
-                        };
-                        
-                    }
-
-                }
-            }
         } else {
-            $logger->debug("No instances found for region : $region");
+            $logger->debug("AssumeRole failed for ARN : $arn");
+            next;
         }
+
+        foreach my $region (@{$targets{$account}}) {
+            # Instances will be retrieved for every region specified line 47
+            if ((my $result = `aws ec2 describe-instances --region $region 2>/dev/null`) ne '') {
+
+                $result = decode_json $result;
+
+                if (@{$result->{Reservations}}) {
+                    $logger->debug("Generating xml data for region : $region");        
+                    # reservations level
+                    foreach my $reservation (@{$result->{Reservations}}) {
+                        my $reservationId = $reservation->{ReservationId};
+                        my $ownerId = $reservation->{OwnerId};
+                        
+                        # instance level
+                        foreach my $instance (@{$reservation->{Instances}}) {
+                            # Add XML
+                            push @{$common->{xmltags}->{AWS_INSTANCES}},
+                            {
+                                RESERVATION_ID => [$reservationId],
+                                OWNER_ID => [$ownerId],
+                                INSTANCE_ID => [$instance->{InstanceId}],
+                                INSTANCE_TYPE => [$instance->{InstanceType}],
+                                LAUNCH_TIME => [$instance->{LaunchTime}],
+                                AVAILABILTY_ZONE => [$instance->{Placement}{AvailabilityZone}],
+                                ARCHITECTURE => [$instance->{Architecture}],
+                                KEY_NAME => [$instance->{KeyName}],
+                                IMAGE_ID => [$instance->{ImageId}],
+                                AMI_LAUNCH_INDEX => [$instance->{AmiLaunchIndex}],
+                                MONITORING => [$instance->{Monitoring}{State}],
+                                STATE_CODE => [$instance->{State}{Code}],
+                                STATE_NAME => [$instance->{State}{Name}],
+                                STATE_REASON => [$instance->{StateTransitionReason}],
+                                VIRTUALIZATION_TYPE => [$instance->{VirtualizationType}],
+                                HIBERNATION_OPT_CONFIGURED => [JSON::PP::is_bool($instance->{HibernationOptions}->{Configured})],
+                                ENCLAVED_OPT_ENABLED => [JSON::PP::is_bool($instance->{EnclaveOptions}->{Enabled})],
+
+                            };
+
+                            foreach my $networkscat (@{$instance->{NetworkInterfaces}}) {
+                                push @{$common->{xmltags}->{AWS_INSTANCES_NETWORKS}},
+                                {
+                                    RESERVATION_ID => [$reservationId],
+                                    OWNER_ID => [$ownerId],
+                                    INSTANCE_ID => [$instance->{InstanceId}],
+                                    MAC_ADDR => [$networkscat->{MacAddress}],
+                                    PRIVATE_DNS_NAME => [$networkscat->{PrivateDnsName}],
+                                    PRIVATE_IP_ADDR => [$networkscat->{PrivateIpAddress}],
+                                    PUBLIC_DNS_NAME => [$networkscat->{PublicDnsName}],
+                                    VPC_ID => [$networkscat->{VpcId}],
+                                    NETWORK_INTERFACE_ID => [$networkscat->{NetworkInterfaceId}],
+                                    SUBNET_ID => [$networkscat->{SubnetId}],
+
+                                };
+
+                            }
+
+
+                            foreach my $blockcat (@{$instance->{BlockDeviceMappings}}) {
+                                push @{$common->{xmltags}->{AWS_INSTANCES_HARDWARE}},
+                                {
+                                    RESERVATION_ID => [$reservationId],
+                                    OWNER_ID => [$ownerId],
+                                    INSTANCE_ID => [$instance->{InstanceId}],
+                                    ROOT_DEVICE_NAME => [$instance->{RootDeviceName}],
+                                    ROOT_DEVICE_TYPE => [$instance->{RootDeviceType}],
+                                    CPU_CORE_COUNT => [$instance->{CpuOptions}{CoreCount}],
+                                    CPU_THREADS_PER_CORE => [$instance->{CpuOptions}{ThreadsPerCore}],
+                                    HYPERVISOR => [$instance->{Hypervisor}],
+                                    BLOCK_DEVICE_NAME => [$blockcat->{DeviceName}],
+                                    # BLOCK_DEVICE_TYPE => $blockcat->{Ebs},
+                                    BLOCK_DEVICE_ATTACH_TIME => [$blockcat->{Ebs}{AttachTime}],
+                                    BLOCK_DEVICE_DELETE_ON_TERMINATION => [JSON::PP::is_bool($blockcat->{Ebs}{DeleteOnTermination})],
+                                    BLOCK_DEVICE_STATUS => [$blockcat->{Ebs}{Status}],
+                                    BLOCK_DEVICE_VOLUME_ID => [$blockcat->{Ebs}{VolumeId}],
+                                };
+                                
+                            }
+
+                        }
+                    }
+                } else {
+                    $logger->debug("No instances found for region : $region");
+                }
+            } else {
+                $logger->debug("Could not access region : $region");
+            }
+
+        }
+
+
     }
-
-
-
 
     $logger->debug("Finishing awsressources_inventory_handler ..");
 }
